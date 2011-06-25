232c12657cda058237e7c22a9137fabb5e9a4eb4
Isaac Hodes <ihodes@mac.com>
2011-06-25 16:50:23 -0700

    Began the shopping module

---
+++ shopping.py
@@ -0,0 +1,17 @@
+# make QUANTITIES a sysem of equalities to make conversion easy & logical
+QUANTITIES = ["lb", "lbs", "pound", "pounds", "oz", "ozs", "ounce", "ounces", \
+                  "cup", "cups", "c", "teaspoon", "teaspoons", "tsp", "t",    \
+                  "tablespoon", "tablespoons"] 
+
+# A cart is a list of cartitems that may be purchased. The prices of items are defined by 
+# items_list a result of parse.parse.
+# 
+# cartitem = {"item":itemname "quantity":quantity}
+# cart = [cartitem]
+
+def calculateCart(cart, items_list):
+    """Takes items and amounts, compares it against the supplied items_list (from parse.parse)
+    and returns the price of the cart"""
+    prices = []
+    for item in cart:
+        if item["item"]
