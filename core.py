78153d48c45528908d3a9b5571732add5aa3e2da
Isaac Hodes <ihodes@mac.com>
2011-06-26 11:40:47 -0700

    began graph work

--- core.py
+++ core.py
@@ -4,11 +4,20 @@ This modules contains methods to manipulate and query the items list.
 item = {item:name quantity:quantity unit:unit_of_quantity price:price}
 items = [item]
 """
-# TODO: make QUANTITIES a sysem of equalities to make conversion easy & logical
+from fractions import Fraction
+
+# TODO need to get rid of this, replace it with the generated graph
+# this is used in tokenizeLineItem to capture units
+# soln: add a nodes list to the graph, require a conversions file to be parsed 
+#       before the items are? cons: slow? need a config file to specify
+#       convrsions.txt file, or default to conversions.txt
 QUANTITIES = ["lb", "lbs", "pound", "pounds", "oz", "ozs", "ounce", "ounces", \
                   "cup", "cups", "c", "teaspoon", "teaspoons", "tsp", "t",    \
                   "tablespoon", "tablespoons"] 
 
+def convertUnit(amount, in_unit, out_unit):
+    """Returns the number of out_units in amount of in_units."""
+    pass
 
 def printItem(item):
     """A nicer way to print out items"""
