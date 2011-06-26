c081d762f6ed737f90a460010d2027540e3dd70c
Isaac Hodes <ihodes@mac.com>
2011-06-26 16:34:15 -0700

    added graph traversal and unit conversion

--- core.py
+++ core.py
@@ -15,10 +15,32 @@ QUANTITIES = ["lb", "lbs", "pound", "pounds", "oz", "ozs", "ounce", "ounces", \
                   "cup", "cups", "c", "teaspoon", "teaspoons", "tsp", "t",    \
                   "tablespoon", "tablespoons"] 
 
-def convertUnit(amount, in_unit, out_unit):
-    """Returns the number of out_units in amount of in_units."""
-    pass
+def traverseWeightedTo(graph, init_weight, in_node, out_node):
+    """Returns the the amount multiplied by the weights between in_node
+    and out_node.
+
+    A breadth-first traversal of the weighted graph from in_node to
+    out_node, multiplying init_weight by the edge weight along the way"""
+    cgraph = graph
+    next_nodes = [] # tuples of new starting nodes, plus new amount
+    for node in cgraph:
+        if node[0] == in_node:
+            new_weight = node[2] * init_weight
+            if node[1] == out_node: # total conversion done
+                return new_weight
+            else: # not done, but don't want to traverse again: 
+                cgraph.remove(node) # remove edge from node
+                cgraph.remove((node[1], node[0], 1/node[2])) # remove edge to
+                next_nodes.append((new_weight, node[1]))
+    for n in next_nodes:
+        final = convertUnit(cgraph, n[0], n[1], out_node)
+        if final: return final
+    return False # no luck
+
+def convertTo(graph, amount, in_node, out_node):
+    return traverseWeightedTo(graph, amount, in_node, out_node)
 
 def printItem(item):
     """A nicer way to print out items"""
-    print "%s: %i %s costs %.2f" % (item["item"], item["quantity"], item["unit"], item["price"])
+    print "%s: %i %s costs %.2f" % (item["item"], item["quantity"], \
+                                        item["unit"], item["price"])
