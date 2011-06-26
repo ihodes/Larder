a50c54cf588e01c58df3cb6ee4ebf6587e150c37
Isaac Hodes <ihodes@mac.com>
2011-06-25 19:45:05 -0700

    fixed some shopping bugs

--- shopping.py
+++ shopping.py
@@ -7,25 +7,29 @@ This module handles the shopping cart.
 # cartitem = {"item":itemname "quantity":quantity}
 # cart = [cartitem] 
 
-def get_item(item_list, item_name):
-    """Returns the item in item_list if it has name=item_name"""
-    for item in item_list:
+def get_item(items, item_name):
+    """Returns the item in items if it has name=item_name"""
+    for item in items:
         if item_name == item["item"]:
             return item
     return None
 
-def calculateCart(cart, items_list):
-    """Takes items and amounts, compares it against the supplied items_list (from parse.parse)
+
+# TODO: take unit type into account (need to do this all over)
+def calculateCart(cart, items):
+    """Takes items and amounts, compares it against the supplied items (from parse.parse)
     and returns the total price of the cart"""
     price = 0
     warnings = "" # in case something isn't in item_list
     for item in cart:
-        iname = item["name"]
+        print item
+        iname = item["item"]
+        print iname
         iquant = item["quantity"]
-        listi = in_list(item_list, iname)
+        listi = get_item(items, iname)
         if listi == None:
-            warnings += "%s is not in the item list.\n" % iname
+            warnings += "\n\t%s is not in the item list." % iname
         else:
             price += (iquant * listi["price"])
     return (price, warnings)
-        
+
