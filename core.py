e66d57acdf72cf4e81b7c1f76ac713e8c33ce5c0
Isaac Hodes <ihodes@mac.com>
2011-06-26 17:54:10 -0700

    fixed conversions via graph traversal

--- core.py
+++ core.py
@@ -21,24 +21,28 @@ def traverseWeightedTo(graph, init_weight, in_node, out_node):
 
     A breadth-first traversal of the weighted graph from in_node to
     out_node, multiplying init_weight by the edge weight along the way"""
-    cgraph = graph
+    edges = graph[1]
+    visited_edges = set()
     next_nodes = [] # tuples of new starting nodes, plus new amount
-    for node in cgraph:
+    for node in edges:
         if node[0] == in_node:
             new_weight = node[2] * init_weight
             if node[1] == out_node: # total conversion done
                 return new_weight
             else: # not done, but don't want to traverse again: 
-                cgraph.remove(node) # remove edge from node
-                cgraph.remove((node[1], node[0], 1/node[2])) # remove edge to
+                visited_edges.add(node)
+                visited_edges.add((node[1], node[0], 1/node[2])) 
                 next_nodes.append((new_weight, node[1]))
+    for n in visited_edges: # don't use used edges
+        edges.discard(n)
     for n in next_nodes:
-        final = convertUnit(cgraph, n[0], n[1], out_node)
+        final = traverseWeightedTo((graph[0], edges), n[0], n[1], out_node)
         if final: return final
     return False # no luck
 
-def convertTo(graph, amount, in_node, out_node):
-    return traverseWeightedTo(graph, amount, in_node, out_node)
+def convertTo(graph, amount, in_units, out_units):
+    """Returns the number of out_units in amount of in_units."""
+    return traverseWeightedTo(graph, amount, in_units, out_units)
 
 def printItem(item):
     """A nicer way to print out items"""
