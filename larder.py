aa448de2289ff724d89369c21d45c67206219d19
Isaac Hodes <ihodes@mac.com>
2011-06-26 17:54:18 -0700

    added more  to debugging

--- larder.py
+++ larder.py
@@ -1,8 +1,9 @@
 import sys, getopt, core, parse, shopping
 
-FLAGS = "hf:spdc:"
+FLAGS = "hf:spdC"
 OPTIONS = ["help", "input-file=", "shopping", "debug-on", "print-items", \
                "conversions="]
+CONVERSIONS_FILE = "conversions.txt"
 
 def main(argv=None):
     print "Larder: -h or --help for information\n"
@@ -10,18 +11,22 @@ def main(argv=None):
         argv = sys.argv
     if not len(argv)>1:
         usage()
+        return 1
     try:
         opts, args = getopt.getopt(argv[1:], FLAGS, OPTIONS)
     except Exception: 
+        print "Error: options are malformed."
         usage()
-
+        return 1
+    
     shop = False
     printing = False
     debug = False
     infile = None
     items = None
-    conversions = None
-    if len(args)>0: infile=open(args[0])
+    try: conversions = open(CONVERSIONS_FILE)
+    except Exception: pass
+    if len(args)>0: infile=open(args[0]) 
 
     for o, a in opts:
         if o in ("-h", "--help"):
@@ -38,10 +43,13 @@ def main(argv=None):
             printing = True
         if o in ("-d", "--debug-on"):
             debug = True
-        if o in ("-c", "--conversions"):
+        if o in ("--conversions"):
             conversions = open(a)
+        if o == "-C":
+            conversions = None
 
-    if infile: items = parse.parse(infile, "norm")
+    if conversions: cgraph = parse.parseConversions(conversions)
+    if infile: items = parse.parse(infile, parseopts="norm", units=cgraph[0])
 
     if shop: 
         scalc = shopping.calculateCart(cart, items)
@@ -53,22 +61,23 @@ def main(argv=None):
     if printing:
         for item in items:
             core.printItem(item)
-        
+
     if debug:
-        print "\nDEBUG ON\n"
+        print "\nDEBUG ON"
         print "opts:", opts, " args: ", args
-        print items
-
-    if conversions:
-        print "doing that"
-        print parse.parseConversions(conversions)
-
+        print "Items:\n", items
+        if conversions:
+            print "\nPrinting conversion graph:"
+            for node in cgraph[0]:
+                print node
+            for edge in cgraph[1]:
+                print edge
     return 0
 
 def usage():
     print
     print "Usage:"
-    print "\tlarder.py [-shdp] [-f] larderfile"
+    print "\tlarder [-shcdp] [-f] larderfile"
     print
     print "Options:"
     print "\t -h, --help \t\t\t\t display this message"
@@ -76,7 +85,8 @@ def usage():
     print "\t -p, --print-items \t\t\t pretty-prints the items from the ingredient file"
     print "\t -d, --debug-on \t\t\t runs larder in debug mode"
     print "\t -s, --shopping \t\t\t reads shopping cart from stdin and prints its price"
-    return 0
+    print "\t -C  \t\t\t\t\t does not convert units"
+    print "\t --conversions <conversionfile>  \t uses <conversionfile> for conversions"
     
 if __name__ == "__main__":
     sys.exit(main())
