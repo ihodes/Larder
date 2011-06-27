c2fa3e82c4df1b1711fed049681d9b416f0583d2
Isaac Hodes <ihodes@mac.com>
2011-06-26 17:42:53 -0700

    small updates

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@ from fractions import Fraction
 
 ING_REGEX = r"([0-9]*/?[0-9]*)\s*(.*)"
 
-def tokenizeLineItem(line):
+def tokenizeLineItem(line, units=None):
     """Splits a larderfile line into proper tokens"""
     raw_line = line.strip().split("=")
     line = []
@@ -29,10 +29,11 @@ def tokenizeLineItem(line):
     unit = ""
 
     # see if the item is modified by a unit type
-    ti = item.split(" ")[0]
-    if ti in core.QUANTITIES: 
-        item = " ".join(item.split(" ")[1:])
-        unit = ti
+    if units:
+        ti = item.split(" ")[0]
+        if ti in units: ## need to fix this: use the conversions file instead of hardcoding
+            item = " ".join(item.split(" ")[1:])
+            unit = ti
 
     return {"item":item, "price":price, "unit":unit, "quantity":quant}
 
@@ -42,42 +43,44 @@ def normalizeItem(item):
     item["quantity"] = 1
     return item
  
-def parse(infile, parseopts=None):
+def parse(infile, parseopts=None, units=None):
     """Returns a list of normalized items."""
     items = []
     for line in infile:
         if len(line)>1:
-            item = tokenizeLineItem(line)
+            item = tokenizeLineItem(line, units)
             if parseopts=="norm": item = normalizeItem(item)
             items.append(item)
-        
     return items
 
 
 def parseConversions(infile, parseopts=None):
-    """Returns a weighted directed  graph to be used for conversions. \
+    """Returns a weighted directed  graph to be used for conversions. 
     Generated from the infile.
 
     infile must have lines of the form: 
         n unit1 = m unit2
     where n and m are arbitrary rational numbers, or blank (=1).
 
-    graph = [edge]
+    graph = [[node], [edge]]
+    node = String
     edge = (node_origin, node_dest, C)
-           where C is the conversion factor between node_origin and \
+           where C is the conversion factor between node_origin and 
            node_dest"""
-    graph = []
+    edges = []
+    nodes = set()
     reg = r"(\d*/?\.?\d+)?\s*(\w+)"
     for line in infile: 
         if len(line)>1:
             splits = line.split("=")
             f = re.match(reg, splits[0]).groups()
             g = re.match(reg, splits[1]).groups()
-            n = m = 1
+            n = m = Fraction(1)
             if f[0]: n = Fraction(f[0])
             if g[0]: m = Fraction(g[0])
-            graph.append((f[1], g[1], m/n))
-            graph.append((g[1], f[1], n/m))
-    return graph
+            nodes.add(f[1])
+            edges.append((f[1], g[1], m/n))
+            edges.append((g[1], f[1], n/m))
+    return (nodes, edges)
     
             
